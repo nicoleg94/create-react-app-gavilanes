935009a7c62b56b8ed03e1d2735b83d4560c2bf1
Nicole Gavilanes <112913726+nicoleg94@users.noreply.github.com>
2023-11-24 20:39:37 -0500

    Cambios en App.js
    Cambios solicitados enclases

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> y guardar para recargar.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.puce.edu.ec/"
           target="_blank"
           rel="noopener noreferrer"
         >
